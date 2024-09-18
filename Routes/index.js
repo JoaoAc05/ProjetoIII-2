eb0b7ab169e3440529533b9e2f4408b7ee82e8d1
João Alves <joaoalvescordeiro2005@gmail.com>
2024-09-18 13:16:46 -0400

    default

--- Routes/index.js
+++ Routes/index.js
@@ -3,6 +3,12 @@ import { AlunoRouter } from "./AlunoRouter.js";
 import { HomeRouter } from "./HomeRouter.js";
 
 const router = express.Router();
+router.get('/', (req, res, next) => {
+    res.json({
+        "statuscode": 200,
+        "sucesso": "Rota default - ProjetoIII V2"
+    });
+});
 router.use("/aluno", AlunoRouter);
 router.use("/home", HomeRouter);
 
