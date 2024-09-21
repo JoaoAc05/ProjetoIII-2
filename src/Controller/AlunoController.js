b5debd25a92bb67b4757fa26c4594c58fb03ead5
João Alves <joaoalvescordeiro2005@gmail.com>
2024-09-21 17:11:20 -0400

    ajustes no put

--- src/Controller/AlunoController.js
+++ src/Controller/AlunoController.js
@@ -6,7 +6,7 @@ class alunoController {
             const alunos = await prisma.Aluno.findMany()
             res.status(200).json(alunos);
         } catch (e) {
-            res.status(500).json({error: 'Erro ao retornar aluno: ' + e});
+            res.status(500).json({error: 'Erro ao retornar aluno: ' + e.message});
         }
     }
 
@@ -22,30 +22,43 @@ class alunoController {
             })
             res.status(200).json(aluno)
         } catch (e) {
-            res.status(500).json({error: 'Erro ao retornar aluno: ' + e})
+            res.status(500).json({error: 'Erro ao retornar aluno: ' + e.message})
         }
     };
 
     async cadastro(req, res, next) {
         try{
             const createAluno = await prisma.Aluno.create({ data: req.body })
-            res.status(200).json({sucesso: 'Aluno criado com sucesso.'});
+            res.status(201).json({sucesso: 'Aluno criado com sucesso.'});
         } catch (e) {
-            res.status(500).json({error: 'Erro ao criar aluno: ' + + e});
+            res.status(500).json({error: 'Erro ao criar aluno: ' + e.message});
         }
     }
 
-    async alterar(req, res, next){
+    async alterar(req, res, next) {
         const { indice } = req.params;
-        try { 
+        const dataToUpdate = req.body; // Sem necessidade de indice no body
+    
+        // Verifica se o body está vazio
+        if (Object.keys(dataToUpdate).length === 0) {
+            return res.status(400).json({ error: 'Nenhum dado fornecido para atualização.' });
+        }
+    
+        try {
             const updateAlunos = await prisma.Aluno.updateMany({
                 where: {
                     indice: Number(indice),
                 },
-            })
-            res.status(200).json({sucesso: 'Aluno alterado com sucesso.'});
+                data: dataToUpdate,  // Passa diretamente o req.body
+            });
+    
+            if (updateAlunos.count === 0) {
+                return res.status(404).json({ error: 'Aluno não encontrado.' });
+            }
+    
+            res.status(200).json({ sucesso: 'Aluno alterado com sucesso.' });
         } catch (e) {
-            res.status(500).json({error:'Erro ao alterar aluno: ' + e});
+            res.status(500).json({ error: 'Erro ao alterar aluno: ' + e.message });
         }
     }
 
@@ -59,7 +72,7 @@ class alunoController {
             })
             res.status(200).json({sucesso: 'Aluno deletado com sucesso.'})
         } catch (e) {
-            res.status(500).json({error: 'Erro ao deletar aluno.'})
+            res.status(500).json({error: 'Erro ao deletar aluno.' + e.message})
         }
     }
 }
