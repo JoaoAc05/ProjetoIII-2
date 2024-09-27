415a516f6f4baf8912780efb5d94f7e2c189b13c
João Alves <joaoalvescordeiro2005@gmail.com>
2024-09-27 12:35:49 -0400

    migrate

--- prisma/migrations/20240927163415_dev/migration.sql
+++ prisma/migrations/20240927163415_dev/migration.sql
@@ -5,7 +5,8 @@ CREATE TABLE "Aluno" (
     "ra" TEXT NOT NULL,
     "cpf" TEXT NOT NULL,
     "imei" INTEGER NOT NULL,
-    "status" INTEGER NOT NULL,
+    "status" INTEGER NOT NULL DEFAULT 0,
+    "hora_post" TIMESTAMP(3),
 
     CONSTRAINT "Aluno_pkey" PRIMARY KEY ("indice")
 );
